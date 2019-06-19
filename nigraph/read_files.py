61fb1be90c5da5a972a330e5e2ca76e7ff4e3c2e
Sarit <a@b.com>
2019-06-19 13:16:02 +0300

    a

---
+++ nigraph/read_files.py
@@ -0,0 +1,47 @@
+import numpy as np
+import nibabel as nib
+import pandas as pd 
+import xml.etree.ElementTree as ET
+import scipy.io as spio
+
+
+def read_nifty_fmri(file_path):
+    nif=nib.load(file_path).get_data()
+    nifshape = nif.shape
+    dtseries = nif.reshape((nifshape[3],nifshape[0]*nifshape[1]*nifshape[2]))
+    return dtseries
+
+
+def read_cifty(file_path):
+    cif=nib.load(file_path).get_data()
+    return cif
+
+
+def read_nifty(file_path):
+    nif=nib.load(file_path).get_data()
+    return nif
+    
+def read_tck(file_path):
+    tck = nib.streamlines.load(file_path)
+    tck = tck.tractogram
+    st = np.array([i for i in tck.streamlines])
+    return st
+
+
+def read_mat(file_path):
+    tracts = spio.loadmat(file_path)
+    tract_data = np.transpose(tracts['Tracts'])
+    for i in range(len(tract_data)):
+        tract_data[i][0] = tract_data[i][0]/tracts['VDims']
+    return tract_data
+
+def read_metadata_atlas(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    ind=[]
+    name=[]
+    for i in root[1]:
+        ind.append(i.attrib['index'])
+        name.append(i.text)
+    return pd.DataFrame(ind,name)
+    
